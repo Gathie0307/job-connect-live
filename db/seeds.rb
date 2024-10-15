e22c72da8c75da2d236443c908cd0987b123154e
Alina Omhandoro <alina.omhandoro@gmail.com>
2024-10-15 17:51:52 +0100

    job seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating job..."
+first_job = Job.new(job_title:"Specialist", company_name:"Cool", company_description:"Cool", company_size:100, location:"Manchester", job_description:"Great", salary:100.00, user_id:user1.id)
+first_job.save!
+
+second_job = Job.new(job_title:"Manager", company_name:"Fine", company_description:"Fine", company_size:10, location:"Edinburgh", job_description:"Fine", salary:50.00, user_id:user2.id)
+second_job.save!
