21ff2690e302c87f7b6e38d8db597f46448080fd
Gayathri Krishnan <iamkgayu@gmail.com>
2024-10-21 17:05:08 +0100

    added the devise setup

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,10 @@
 class JobsController < ApplicationController
   def index
-    @jobs = Job.all
-
+    if current_user.employer
+      @jobs = Job.where(user: current_user)
+    else
+      @jobs = Job.all
+    end
     if params[:query].present?
       @jobs = @jobs.where("job_title ILIKE ?", "%#{params[:query]}%")
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ User.destroy_all
 
 
 
-user1 = User.create!(email: "max@gmail.com", password: "123456")
-user2 = User.create!(email: "alex@gmail.com", password: "123456")
+user1 = User.create!(email: "max@gmail.com", password: "123456", employer: true)
+user2 = User.create!(email: "alex@gmail.com", password: "123456", employer: false)
 user3 = User.create!(email: "sue@gmail.com", password: "123456")
 user4 = User.create!(email: "clair@gmail.com", password: "123456")
 
