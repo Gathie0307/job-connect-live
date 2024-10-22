dc1b3f9fd50768724f24302f12ef742222491642
Marcelo Mancini <mancini2007@gmail.com>
2024-10-22 11:47:18 +0100

    fixed cloudinary

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ApplicationRecord
   has_many :favourites
   has_many :job_applications
 
+  has_one_attached :photo
   # We are using Cloudinary
   # mount_uploader :image, ImageUploader
 end
