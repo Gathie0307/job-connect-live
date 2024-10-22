07c388c61d708b93e17b50a299f84629f365b8ff
Alina Omhandoro <alina.omhandoro@gmail.com>
2024-10-22 12:08:55 +0100

    message model and chat styling added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_21_141904) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_22_103739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_141904) do
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "job_application_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_application_id"], name: "index_messages_on_job_application_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -90,4 +100,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_141904) do
   add_foreign_key "job_applications", "jobs"
   add_foreign_key "job_applications", "users"
   add_foreign_key "jobs", "users"
+  add_foreign_key "messages", "job_applications"
+  add_foreign_key "messages", "users"
 end
