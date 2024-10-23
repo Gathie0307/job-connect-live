ccf966ba9a2f91919a9950c72bdf055a727c6592
Gayathri Krishnan <iamkgayu@gmail.com>
2024-10-23 15:58:37 +0100

    Solution without Solidcable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,14 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :job_application
-  after_create_commit :broadcast_message
+  # after_create_commit :broadcast_message
 
-  private
+  # private
 
-  def broadcast_message
-    broadcast_append_to "job_application_#{job_application.id}_messages",
-                        partial: "messages/message",
-                        locals: { message: self, user: user },
-                        target: "messages"
-  end
+  # def broadcast_message
+  #   broadcast_append_to "job_application_#{job_application.id}_messages",
+  #                       partial: "messages/message",
+  #                       locals: { message: self, user: user },
+  #                       target: "messages"
+  # end
 end
