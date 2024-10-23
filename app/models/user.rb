67e25c5dad655515f23fdef9ea44171763abcde9
Marcelo Mancini <mancini2007@gmail.com>
2024-10-23 14:25:52 +0100

    link saved job btn to fav

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :job_applications
   has_many :jobs, through: :favourites
-  has_many :jobs
+  has_many :favourites
 end
