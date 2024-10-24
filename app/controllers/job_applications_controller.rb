dbcb144fa7154a216f9d7f636090bdd77eee9ea4
Gayathri Krishnan <iamkgayu@gmail.com>
2024-10-24 15:38:54 +0100

    Fixed Bugs

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -2,7 +2,6 @@ class JobApplicationsController < ApplicationController
   before_action :authenticate_user!
   def index
     if current_user.employer == true
-      raise
       @pending_applications = current_user.jobs.each { |job| job.job_applications.where(status: "pending", user: !current_user)}
       @confirmed_applications = current_user.jobs.each { |job| job.job_applications.where(status: "confirmed", user: !current_user)}
      # @pending_applications = current_user.job_applications.where(status: "pending")
